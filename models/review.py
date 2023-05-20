fa262c80662132e34b7b6cf501be0fe119dc2474
OptimisticUnderdog <mabunda.wealth@gmail.com>
2023-05-20 10:52:06 -0700

    AirBnB_clone_v2

--- models/review.py
+++ models/review.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from sqlalchemy import Column, Integer, String, ForeignKey
+
+import models
 from models.base_model import BaseModel, Base
 from os import getenv
-from sqlalchemy.orm import relationship
-
-STORAGE = getenv("HBNB_TYPE_STORAGE")
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 
 
 class Review(BaseModel, Base):
-    """ Review class to store review information """
-    __tablename__ = "reviews"
-    if STORAGE == "db":
+    """Representation of Review """
+    if models.storage_t == 'db':
+        __tablename__ = 'reviews'
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         text = Column(String(1024), nullable=False)
-
     else:
         place_id = ""
         user_id = ""
         text = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Review"""
+        super().__init__(*args, **kwargs)
