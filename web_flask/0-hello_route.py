3919720b49d0e9b392dede239df5234b080b75dd
OptimisticUnderdog <mabunda.wealth@gmail.com>
2023-06-15 11:54:45 +0000

    correction

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,15 +3,14 @@
 Configures content to serve on port 5000
 """
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/airbnb-onepage/', methods=['GET'])
-def hello_route():
-    return "Hello HBNB!"
-
+@app.route('/', strict_slashes=False)
+def hello():
+    return render_template("10-hbnb_filters.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=None)
